bbf3865aa6d4108642246fc0ec0833d67bf2853e
Priyanka Srivastava <srivastavapriyanka71307@gmail.com>
2026-03-08 16:52:09 +0530

    implement NoteList for displaying notes and NotesCount using useMemo

---
+++ react-notes-dashboard/src/components/NotesCount.jsx
@@ -0,0 +1,10 @@
+import { useContext, useMemo } from "react";
+import { NotesContext } from "../context/NotesContext";
+
+function NotesCount(){
+    const {notes} = useContext(NotesContext);
+    const total = useMemo(()=> notes.length, [notes]);
+    return <h3>Total Notes: {total}</h3>
+}
+
+export default NotesCount;
---
+++ react-notes-dashboard/src/components/NotesList.jsx
@@ -0,0 +1,23 @@
+import { useContext } from "react";
+import { NotesContext } from "../context/NotesContext";
+
+function NotesList(){
+    const { notes, selected, setSelected } = useContext(NotesContext);
+    return(
+        <ul>
+            {notes.map((note, index)=>(
+                <li key={index}
+                onClick={()=> setSelected(index)}
+                style={{
+                    cursor: "pointer",
+                    background: selected === index ? "yellow" : "white",
+                    padding: "5px"
+                }}>
+                    {note}
+                </li>
+            ))}
+        </ul>
+    );
+}
+
+export default NotesList;
